1c7757ebd196f350dc31891a22fe7a28bd1805f4
Sujeet <sujeet4work@gmail.com>
2026-09-10 10:49:06 -0700

    feat: add flywheel CLI skeleton with version and init

---
+++ cmd/flywheel/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"flywheel/internal/flywheel"
+)
+
+const version = "v0.1.0-dev"
+
+func main() {
+	if len(os.Args) < 2 {
+		usage(os.Stderr)
+		os.Exit(2)
+	}
+	switch os.Args[1] {
+	case "version":
+		runVersion(os.Args[2:])
+	case "init":
+		runInit(os.Args[2:])
+	default:
+		fmt.Fprintf(os.Stderr, "flywheel: unknown subcommand %q\n", os.Args[1])
+		usage(os.Stderr)
+		os.Exit(2)
+	}
+}
+
+func usage(w io.Writer) {
+	fmt.Fprintf(w, "usage: flywheel <subcommand>\n\nsubcommands:\n  init      scaffold flywheel state files into a directory\n  version   print the flywheel version\n")
+}
+
+func runVersion(args []string) {
+	fs := flag.NewFlagSet("version", flag.ContinueOnError)
+	fs.SetOutput(io.Discard)
+	if err := fs.Parse(args); err != nil {
+		fmt.Fprintf(os.Stderr, "flywheel version: %v\n", err)
+		usage(os.Stderr)
+		os.Exit(2)
+	}
+	if fs.NArg() > 0 {
+		fmt.Fprintf(os.Stderr, "flywheel version: unexpected argument %q\n", fs.Arg(0))
+		usage(os.Stderr)
+		os.Exit(2)
+	}
+	fmt.Println("flywheel " + version)
+}
+
+func runInit(args []string) {
+	fs := flag.NewFlagSet("init", flag.ContinueOnError)
+	fs.SetOutput(io.Discard)
+	dir := fs.String("dir", ".", "target directory (default: current working directory)")
+	force := fs.Bool("force", false, "overwrite an existing flywheel.md")
+	if err := fs.Parse(args); err != nil {
+		fmt.Fprintf(os.Stderr, "flywheel init: %v\n", err)
+		usage(os.Stderr)
+		os.Exit(2)
+	}
+	if fs.NArg() > 0 {
+		fmt.Fprintf(os.Stderr, "flywheel init: unexpected argument %q\n", fs.Arg(0))
+		usage(os.Stderr)
+		os.Exit(2)
+	}
+	path, err := flywheel.Init(*dir, *force)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "flywheel init: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(path)
+}
